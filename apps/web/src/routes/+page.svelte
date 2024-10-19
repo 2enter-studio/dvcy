9cf445eb357ec3fd2031e21f06b2b9850d62e506
davilivob <david.iii.chiu@gmail.com>
2024-10-19 21:10:46 +0800

    [apps/web] add intro page for language selection

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Locale } from '@/config';
+
+	const routes: { name: string; locale: Locale }[] = [
+		{ name: '中文', locale: 'zh-tw' },
+		{ name: 'English', locale: 'en' }
+	];
+</script>
+
+<div class="full-screen flex flex-col items-center justify-between text-center">
+	<div class=""></div>
+	<div>
+		<h3>請選擇語言</h3>
+		<h3 class="mb-3">Pick a language</h3>
+		<div class="flex justify-center gap-3">
+			{#each routes as { name, locale }}
+				<a href="/{locale}">{name}</a>
+			{/each}
+		</div>
+	</div>
+
+	<div class="mb-20 text-6xl font-extrabold tracking-wider">
+		<h1 class="text-6xl">CHIAYI</h1>
+		<h2 class="text-4xl">DATA-VERSE</h2>
+	</div>
+</div>
