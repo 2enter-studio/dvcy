ae55d9cd4538b224d8cdadae3189bda7bdd86e6a
davilivob <david.iii.chiu@gmail.com>
2024-10-21 17:34:01 +0800

    [web] crop image when loading /[locale]/[id]

--- apps/web/src/routes/[locale]/[id]/+page.server.ts
+++ apps/web/src/routes/[locale]/[id]/+page.server.ts
@@ -1,11 +1,20 @@
+import sharp from 'sharp';
+import bufferToDataUrl from 'buffer-to-data-url';
+
 import { pb } from '@/server/pb';
 import { SignImage } from '@repo/lib/pb';
+import { SIGN_SIZES } from '@/config';
 
 export const load = async ({ params }) => {
 	const { id } = params;
 
 	const image = new SignImage(pb, id);
-	const imgUrl = await image.getUrl();
+	const buffer = await image.getBuffer();
+	const type = await image.getType();
+	if (!type || !buffer) return {};
+
+	const resized = await sharp(buffer).resize(SIGN_SIZES[type][0], SIGN_SIZES[type][1], { position: 'center' }).toBuffer();
+	const imgUrl = await bufferToDataUrl('image/jpeg', resized);
 
 	return { imgUrl, id };
 };
