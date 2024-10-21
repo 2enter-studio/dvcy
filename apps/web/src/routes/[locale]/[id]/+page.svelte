e6f445205ab2d1a865e348f466cfd514ebccffa4
davilivob <david.iii.chiu@gmail.com>
2024-10-21 17:34:32 +0800

    [web] testing code for detecting device orientation

--- apps/web/src/routes/[locale]/[id]/+page.svelte
+++ apps/web/src/routes/[locale]/[id]/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { localizations } from '@/localization';
 	import { browser } from '$app/environment';
+	import eruda from 'eruda';
 
 	const { data } = $props();
 	const { id, imgUrl, locale, source } = data;
@@ -10,6 +11,15 @@
 
 	onMount(() => {
 		if (!browser) return;
+		eruda.init();
+		if (window.DeviceOrientationEvent) {
+			console.log('start listening to device orientation');
+			window.addEventListener('deviceorientation', (e) => {
+				console.log(e.beta, e.alpha, e.gamma);
+			});
+		} else {
+			console.warn('can not detect device orientation');
+		}
 		if (!document.querySelector('a')) {
 			window.location.reload();
 		}
@@ -22,7 +32,7 @@
 >
 	<div class="flex h-full flex-col items-center justify-between border-[1.5rem] border-solid border-secondary/50 bg-primary/40 py-12">
 		<h1 class="text-center text-3xl font-bold tracking-wide">{localization.downloadYourSign}</h1>
-		<img src={imgUrl} alt="" class="w-11/12 rounded-xl border-4 border-black" />
+		<img src={imgUrl} alt="your sign" class="max-h-[60%] max-w-[93%] rounded-lg border-4 border-black shadow-xl shadow-black/40" />
 		<a href="/{locale}?source={source}" class="btn btn-secondary">{localization.back}</a>
 	</div>
 </div>
