81d94ec1cf01fb191537cda8add34408db22548a
davilivob <david.iii.chiu@gmail.com>
2024-10-21 11:41:44 +0800

    [apps/web] add const: SIGN_SIZE, ROUTES

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,9 +1,14 @@
 const SIGN_TYPES = [0, 1, 2, 3, 4] as const;
 const LOCALES = ['en', 'zh-tw'] as const;
 const DEFAULT_LOCALE = 'zh-tw';
+const SIGN_SIZE = 2048;
+const ROUTES = [
+	{ name: '中文', locale: 'zh-tw' },
+	{ name: 'English', locale: 'en' }
+] as const;
 
 type SignType = (typeof SIGN_TYPES)[number];
 type Locale = (typeof LOCALES)[number];
 
-export { SIGN_TYPES, LOCALES, DEFAULT_LOCALE };
+export { SIGN_TYPES, LOCALES, DEFAULT_LOCALE, SIGN_SIZE, ROUTES };
 export type { SignType, Locale };
